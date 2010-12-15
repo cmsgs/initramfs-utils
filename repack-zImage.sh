dbd2e0f287651b9218a81f70a9d7d8ca02b12727
Gary Danko <gdanko@gmail.com>
2010-12-14 22:14:17 -0800

    added error checkig, build environment not hard coded

--- repack-zImage.sh
+++ repack-zImage.sh
@@ -7,8 +7,10 @@
 ##############################################################################
 set -x
 # you should point where your cross-compiler is
-COMPILER_PATH="${HOME}/arm-none-eabi-4.3.4/bin"
-COMPILER="$COMPILER_PATH/arm-none-eabi"
+COMPILER_PATH="${HOME}/gnuarm-4.0.2/bin"
+COMPILER_PREFIX="arm-elf"
+COMPILER="${COMPILER_PATH}/${COMPILER_PREFIX}"
+VERSION=`ls -1 $COMPILER_PATH/../lib/gcc/${COMPILER_PREFIX}`
 ##############################################################################
 
 zImage=$1
@@ -25,7 +27,7 @@ DATE=`date`
 test -f ./locals.sh && source ./locals.sh
 
 # you should point this where your AOSP root is in the environment
-[ -z "$AOSP" ] && AOSP="/data/android/cm"
+[ -z "$AOSP" ] && AOSP="/data/android_build/mydroid"
 
 write_script() {
     test -n "$TARGET_DEVICE_NAME" && \
@@ -134,7 +136,7 @@ pushd $new_ramdisk_dir
 find ./ | grep -v ".gitignore" | cpio -o -H newc > $OUT/new_ramdisk.cpio
 popd
 
-# Check the new ramdispk's size
+# Check the new ramdipk's size
 ramdsize=`ls -l out/new_ramdisk.cpio | awk '{print $5}'`
 echo "##### 06. The size of the new ramdisk is = $ramdsize"
 
@@ -180,19 +182,19 @@ gzip -f -9 < arch/arm/boot/compressed/../Image > arch/arm/boot/compressed/piggy.
 
 #2. piggy.gz -> piggy.o
 echo "##### 12. piggy.gz ---> piggy.o"
-$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.piggy.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/arm-none-eabi/4.3.4/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8  -msoft-float -gdwarf-2  -Wa,-march=all   -c -o arch/arm/boot/compressed/piggy.o arch/arm/boot/compressed/piggy.S
+$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.piggy.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/${COMPILER_PREFIX}/${VERSION}/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8  -msoft-float -gdwarf-2  -Wa,-march=all   -c -o arch/arm/boot/compressed/piggy.o arch/arm/boot/compressed/piggy.S
 
 #3. head.o
 echo "##### 13. Compiling head"
-$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.head.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/arm-none-eabi/4.3.4/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8  -msoft-float -gdwarf-2  -Wa,-march=all   -c -o arch/arm/boot/compressed/head.o arch/arm/boot/compressed/head.S
+$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.head.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/${COMPILER_PREFIX}/${VERSION}/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -D__ASSEMBLY__ -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8  -msoft-float -gdwarf-2  -Wa,-march=all   -c -o arch/arm/boot/compressed/head.o arch/arm/boot/compressed/head.S
 
 #4. misc.o
 echo "##### 14. Compiling misc"
-$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.misc.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/arm-none-eabi/4.3.4/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Os -marm -fno-omit-frame-pointer -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8 -msoft-float -Uarm -fno-stack-protector -I/modules/include -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -Wdeclaration-after-statement -Wno-pointer-sign -fwrapv -fpic -fno-builtin -Dstatic=  -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(misc)"  -D"KBUILD_MODNAME=KBUILD_STR(misc)"  -c -o arch/arm/boot/compressed/misc.o arch/arm/boot/compressed/misc.c
+$COMPILER-gcc -Wp,-MD,arch/arm/boot/compressed/.misc.o.d  -nostdinc -isystem $COMPILER_PATH/../lib/gcc/${COMPILER_PREFIX}/${VERSION}/include -Dlinux -Iinclude  -Iarch/arm/include -include include/linux/autoconf.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-s5pc110/include -Iarch/arm/plat-s5pc11x/include -Iarch/arm/plat-s3c/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Os -marm -fno-omit-frame-pointer -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a8 -msoft-float -Uarm -fno-stack-protector -I/modules/include -fno-omit-frame-pointer -fno-optimize-sibling-calls -g -Wdeclaration-after-statement -Wno-pointer-sign -fwrapv -fpic -fno-builtin -Dstatic=  -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(misc)"  -D"KBUILD_MODNAME=KBUILD_STR(misc)"  -c -o arch/arm/boot/compressed/misc.o arch/arm/boot/compressed/misc.c
 
 #5. head.o + misc.o + piggy.o --> vmlinux
 echo "##### 15. head.o + misc.o + piggy.o ---> vmlinux"
-$COMPILER-ld -EL    --defsym zreladdr=0x30008000 --defsym params_phys=0x30000100 -p --no-undefined -X $COMPILER_PATH/../lib/gcc/arm-none-eabi/4.3.4/libgcc.a -T arch/arm/boot/compressed/vmlinux.lds arch/arm/boot/compressed/head.o arch/arm/boot/compressed/piggy.o arch/arm/boot/compressed/misc.o -o arch/arm/boot/compressed/vmlinux 
+$COMPILER-ld -EL    --defsym zreladdr=0x30008000 --defsym params_phys=0x30000100 -p --no-undefined -X $COMPILER_PATH/../lib/gcc/${COMPILER_PREFIX}/${VERSION}/libgcc.a -T arch/arm/boot/compressed/vmlinux.lds arch/arm/boot/compressed/head.o arch/arm/boot/compressed/piggy.o arch/arm/boot/compressed/misc.o -o arch/arm/boot/compressed/vmlinux 
 
 #6. vmlinux -> zImage
 echo "##### 16. vmlinux ---> zImage"
@@ -200,6 +202,11 @@ $COMPILER-objcopy -O binary -R .note -R .note.gnu.build-id -R .comment -S  arch/
 
 popd
 mv $KSRC/arch/arm/boot/zImage out/zImage
+echo "Copying kernel to /data/android_build/kernel-images/zImage-${TITLE}"
+cp $KSRC/arch/arm/boot/zImage out/zImage /data/android_build/kernel-images/zImage-${TITLE}
+
+# I don't care about Odin
+exit
 
 echo "##### 17. Creating zImage-inject.tar for Odin"
 tar c -C out zImage > zImage-inject.tar
